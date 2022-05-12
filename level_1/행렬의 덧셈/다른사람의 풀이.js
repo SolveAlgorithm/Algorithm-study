a4f54fed05b373de4b82ab6735276df5a0deeecf
HAWNBINYOO <dngh0825@naver.com>
2022-05-12 23:47:37 +0900

    5/12/23

---
+++ level_1/행렬의 덧셈/다른사람의 풀이.js
@@ -0,0 +1,6 @@
+const matrixAddition = (matrix1, matrix2) =>
+  matrix1.map((row, y) => row.map((v, x) => v + matrix2[y][x]));
+
+function solution(arr1, arr2) {
+  return matrixAddition(arr1, arr2);
+}
