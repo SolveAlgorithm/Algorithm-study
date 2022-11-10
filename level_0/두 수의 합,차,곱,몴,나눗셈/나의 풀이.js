22111be393ae21007192f6b68145f18f19230d86
hawnbin <dngh0825@naver.com>
2022-11-10 10:08:44 +0900

    Day 1

---
+++ level_0/두 수의 합,차,곱,몴,나눗셈/나의 풀이.js
@@ -0,0 +1,24 @@
+// 두 수의 덧셈
+function solution(num1, num2) {
+    return num2 + num1;
+}
+
+// 두 수의 뺼셈
+// function solution(num1, num2) {
+//     return num1 - num2
+// }
+
+// 두 수의 곱
+// function solution(num1, num2) {
+//     return num1 * num2;
+// }
+
+// 두 수의 몴
+// function solution(num1, num2) {
+//     return Math.floor(num1 / num2) ;
+// }
+
+//나머지 * 1000
+// function solution(num1, num2) {   
+//     return Math.floor((num1 / num2) * 1000);
+// }
---
+++ level_0/두 수의 합,차,곱,몴,나눗셈/다른 사람의 풀이.js
@@ -0,0 +1,9 @@
+const solution = (num1, num2) => num1 + num2
+
+//const solution = (num1, num2) => num1 - num2
+
+//const solution = (num1, num2) => num1 * num2
+
+// const solution = (num1, num2) => Math.floor(num1 / num2)
+
+// const solution = (num1, num2) => Math.floor(num1 / num2 * 1000)
