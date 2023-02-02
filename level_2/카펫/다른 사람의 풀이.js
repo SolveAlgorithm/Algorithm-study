dcbd20fc3b6bcd98cbb856161193ac9b7d7838fe
HWANBINYOO <dngh0825@naver.com>
2023-02-02 20:06:22 +0900

    20 : 06

---
+++ level_2/카펫/다른 사람의 풀이.js
@@ -0,0 +1,9 @@
+function solution(brown, yellow) {
+    for (var i = 3; i <= (brown+yellow)/i; i++) {
+        var x = Math.floor((brown+yellow)/i);
+        if( (x-2)*(i-2)=== yellow) {
+            break;
+        }
+    }
+    return [x,i];
+}
